c97bdcd3595405d02f2d1b498a2609b3c19eda2a
ginger-kang <kdhoon07@gmail.com>
2020-10-22 18:07:34 +0900

    :sparkles: Login Setup and show Error Message

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
 
   const onChange = (event) => {
     const {target: {name, value}} = event;
@@ -27,17 +28,20 @@ const Auth = () => {
       }
       console.log(data);
     } catch(error) {
-      console.log(error)
+      setError(error.message);
     }
   }
+  const toggleAccount = () => setNewAccount((prev) => !prev);
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
         <input name="password" type="password" placeholder="password" required value={password} onChange={onChange}/>
-        <input type="submit" value={newAccount ? "Create Account" : "Login"} />
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+        {error} 
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
