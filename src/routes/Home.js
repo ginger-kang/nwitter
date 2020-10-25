b7fed78033f649fda7ce1e09635279c0f7ac2a47
ginger-kang <kdhoon07@gmail.com>
2020-10-25 20:34:59 +0900

    :speech_balloon: Nweeting using firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,32 @@
 import React, { useState } from 'react';
+import { dbService } from 'fBase';
 
 const Home = () => {
   const [nweet, setNweet] = useState('');
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-  }
+    // Create document
+    await dbService.collection('nweets').add({
+      nweet,
+      createdAt: Date.now()
+    });
+    setNweet('');
+  };
 
   const onChange = (event) => {
     const {target: {value}} = event;
     setNweet(value);
-  }
+  };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input name={nweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
+        <input value={nweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
         <input type="submit" value="Nweet" />
       </form>
     </div>
   );
-}
+};
+
 export default Home;
