a62c90c0e595c1013485d9fdb2434852b3196120
ginger-kang <kdhoon07@gmail.com>
2020-10-29 23:34:09 +0900

    :sparkles: Preview Upload image

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Nweet from 'components/Nweet';
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService.collection('nweets').onSnapshot((snapshot) => {
@@ -38,9 +39,14 @@ const Home = ({ userObj }) => {
     const file = files[0]; 
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
-      console.log(finishedEvent);
+      const { currentTarget: { result }} = finishedEvent;
+      setAttachment(result);
     }
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(file); 
+  }
+
+  const onClearAttachment = () => {
+    setAttachment(null);
   }
   //console.log(nweets);
   return (
@@ -49,6 +55,12 @@ const Home = ({ userObj }) => {
         <input value={nweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
         <input type="file" accept="image/*" onChange={onFileChange}/>
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="attachment" width="50px" height="50px" />
+            <button onClick={onClearAttachment}>업로드 취소</button>
+          </div>
+        )}
       </form> 
       <div>
         {nweets.map((nweet) => (
